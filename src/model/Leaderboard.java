e5160af635f54efae343670b9ccc556368553708
Mattia Cavalieri Manasse <cavalierimanasse.2010869@strudenti.uniroma1.it>
2025-03-20 14:04:43 +0100

    added Leaderboard and Menu Classes

---
+++ src/model/Leaderboard.java
@@ -0,0 +1,71 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Leaderboard {
+    private List<LeaderboardRecord> records;
+    private static final String FILE_PATH = "";
+
+    public Leaderboard() {
+        records = new ArrayList<LeaderboardRecord>();
+    }
+
+    public void setRecords(List<LeaderboardRecord> records) {
+        this.records = records;
+        Collections.sort(records);
+    }
+
+    public List<LeaderboardRecord> getTopTenRecords() {
+        if (records.size() < 10) {
+            return records;
+        }
+        return records.subList(0, 10);
+    }
+
+    public class LeaderboardRecord implements Comparable<LeaderboardRecord> {
+        private String nickname;
+        private String avatar;
+        private int maxScore;
+        private int maxLevel;
+
+        public LeaderboardRecord(String nickname, String avatar, int maxScore, int maxLevel) {
+            this.nickname = nickname;
+            this.avatar = avatar;
+            this.maxScore = maxScore;
+            this.maxLevel = maxLevel;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        public String getAvatar() {
+            return avatar;
+        }
+
+        public int getMaxScore() {
+            return maxScore;
+        }
+
+        public int getMaxLevel() {
+            return maxLevel;
+        }
+
+        @Override
+        public int compareTo(LeaderboardRecord o) {
+            int compared = Integer.compare(this.getMaxScore(), o.getMaxScore());
+            if (compared == 0) {
+                compared = Integer.compare(this.getMaxLevel(), o.getMaxLevel());
+            }
+            return compared;
+        }
+
+        @Override
+        public String toString() {
+            return nickname + ", " + avatar + ", " + maxScore + ", " + maxLevel;
+        }
+    }
+
+}
---
+++ src/model/Menu.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Menu extends AbstractModel {
+    private int selectedItem;
+
+    public int getSelectedItem() {
+        return selectedItem;
+    }
+
+    public void setSelectedItem(int selectedItem) {
+        this.selectedItem = selectedItem;
+    }
+}
